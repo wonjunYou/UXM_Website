5735d5e7bcc9cd3fed0af1f59c574e37ee0213c2
sc-habrush24 <sol3024@naver.com>
2020-09-11 02:51:37 +0900

    200911

--- uxmweb_app/views.py
+++ uxmweb_app/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import render
 def publication(request):
     dojin = "dojin"
     return render(request, 'publication.html',{"test":dojin})
-    
+def project(request):
+    pj = "pj"
+    return render(request, 'project.html',{"pj":pj})
 def main(request):
     return render(request, 'main.html')
